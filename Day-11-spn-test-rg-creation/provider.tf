7de96821b6e356357e158687276a06abfa5fbe27
Ram <ram.sundar14194@gmail.com>
2026-05-24 13:04:02 +0530

    day 11 added

---
+++ Day-11-spn-test-rg-creation/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.62.1"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  client_id = "def23930-6220-450b-ba15-9e6b514a8438"
+  client_secret = "z2Y8Q~eiwYZeN5lxbXnbphD1EyHFQ6~pDGF-kbjB"
+  tenant_id = "4da0c7c8-386d-4699-b2d6-1bf91c67ed24"
+  subscription_id = "48662256-1551-4a3d-8e37-335744834f2e"
+}
